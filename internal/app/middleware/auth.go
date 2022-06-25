d3141fb56c4e2dab5fcd1a62722af1ec1f3d439c
Alex Rundquist <alrund@gmail.com>
2022-06-25 22:48:59 +0300

    Add cookies

--- internal/app/middleware/auth.go
+++ internal/app/middleware/auth.go
@@ -2,7 +2,9 @@ package middleware
 
 import (
 	"context"
+	"errors"
 	"net/http"
+	"time"
 
 	"github.com/google/uuid"
 )
@@ -13,7 +15,37 @@ const UserIDContextKey ContextKey = "userID"
 
 func Auth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := context.WithValue(r.Context(), UserIDContextKey, uuid.New().String())
+		userID, err := GetCookie(r)
+		if err != nil && !errors.Is(err, http.ErrNoCookie) {
+			http.Error(w, "500 Internal Server Error.", http.StatusInternalServerError)
+			return
+		}
+		
+		if userID == "" {
+			userID = uuid.New().String()
+			AddCookie(userID, w)
+		}
+
+		ctx := context.WithValue(r.Context(), UserIDContextKey, userID)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
+}
+
+func AddCookie(userID string, w http.ResponseWriter) {
+	http.SetCookie(w, &http.Cookie{
+		Name:     string(UserIDContextKey),
+		Value:    userID,
+		Path:     "/",
+		Expires:  time.Now().Add(365 * 24 * time.Hour),
+		HttpOnly: true,
+	})
+}
+
+func GetCookie(r *http.Request) (string, error) {
+	userCookie, err := r.Cookie(string(UserIDContextKey))
+	if err != nil {
+		return "", err
+	}
+
+	return userCookie.Value, nil
 }
