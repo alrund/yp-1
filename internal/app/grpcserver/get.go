326a66f790b63d24f4879acf15436a66f6f54bdc
Alex Rundquist <alrund@gmail.com>
2022-10-23 15:39:49 +0300

    Add grpc method Get with test

---
+++ internal/app/grpcserver/get.go
@@ -0,0 +1,52 @@
+package grpcserver
+
+import (
+	"context"
+	"errors"
+	"github.com/alrund/yp-1/internal/app/token"
+	"net/http"
+
+	"github.com/alrund/yp-1/internal/app/storage"
+	pb "github.com/alrund/yp-1/internal/proto"
+)
+
+func (s *Server) Get(ctx context.Context, in *pb.GetRequest) (*pb.GetResponse, error) {
+	var response pb.GetResponse
+
+	if in.Token == "" {
+		response.ErrorCode = http.StatusBadRequest
+		response.Error = http.StatusText(http.StatusBadRequest)
+		return &response, nil
+	}
+
+	url, err := s.us.Get(in.Token)
+	if err != nil {
+		if errors.Is(err, storage.ErrTokenNotFound) {
+			response.ErrorCode = http.StatusNotFound
+			response.Error = http.StatusText(http.StatusNotFound)
+			return &response, nil
+		}
+
+		if errors.Is(err, token.ErrTokenExpiredError) {
+			response.ErrorCode = 498
+			response.Error = "498 Invalid Token."
+			return &response, nil
+		}
+
+		if errors.Is(err, token.ErrTokenRemovedError) {
+			response.ErrorCode = http.StatusGone
+			response.Error = http.StatusText(http.StatusGone)
+			return &response, nil
+		}
+
+		response.ErrorCode = http.StatusInternalServerError
+		response.Error = http.StatusText(http.StatusInternalServerError)
+		return &response, nil
+	}
+
+	response.ErrorCode = http.StatusTemporaryRedirect
+	response.Error = http.StatusText(http.StatusTemporaryRedirect)
+	response.Url = url
+
+	return &response, nil
+}
