5c787c684ae92cf7c7a351e3e14cfe153e7afe84
Alex Rundquist <alrund@gmail.com>
2022-10-25 15:50:42 +0300

    Remove AddJSON method

--- internal/app/grpcserver/add.go
+++ internal/app/grpcserver/add.go
@@ -2,7 +2,6 @@ package grpcserver
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"net/http"
 
@@ -42,50 +41,6 @@ func (s *Server) Add(ctx context.Context, in *pb.AddRequest) (*pb.AddResponse, e
 	}
 
 	response.ShortUrl = s.us.GetBaseURL() + token.Value
-
-	return &response, nil
-}
-
-// AddJSON adds a URL string to shorten as a JSON object.
-func (s *Server) AddJSON(ctx context.Context, in *pb.AddJSONRequest) (*pb.AddJSONResponse, error) {
-	var response pb.AddJSONResponse
-	response.Code = http.StatusCreated
-
-	contextUserID := ctx.Value(UserIDContextKey)
-	userID, ok := contextUserID.(string)
-	if !ok {
-		response.Code = http.StatusInternalServerError
-		response.Message = http.StatusText(http.StatusInternalServerError)
-		return &response, nil
-	}
-
-	jsonRequest := JSONRequest{}
-	err := json.Unmarshal([]byte(in.UrlJson), &jsonRequest)
-	if err != nil {
-		response.Code = http.StatusBadRequest
-		response.Message = err.Error()
-		return &response, err
-	}
-
-	token, err := s.us.Add(userID, jsonRequest.URL)
-	if err != nil {
-		if !errors.Is(err, storage.ErrURLAlreadyExists) {
-			response.Code = http.StatusInternalServerError
-			response.Message = err.Error()
-			return &response, err
-		}
-		response.Code = http.StatusConflict
-	}
-
-	jsonResponse := JSONResponse{Result: s.us.GetBaseURL() + token.Value}
-	result, err := json.Marshal(jsonResponse)
-	if err != nil {
-		response.Code = http.StatusInternalServerError
-		response.Message = err.Error()
-		return &response, err
-	}
-
-	response.ShortUrlJson = string(result)
 
 	return &response, nil
 }
